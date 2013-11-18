fe7fc2301140dfc60c0148c4f0878861b37147ea
Marvin Hoffman <divermarv@gmail.com>
2013-11-17 20:58:26 -0600

    Doing Lesson 8

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 /**
  * Created by marvinhoffman on 11/15/13.
  */
-function FirstCtrl($scope) {
-    $scope.data = {message: "button"};
+var myApp = angular.module("myApp", []);
+myApp.factory('Data', function() {
+    return {message: "I'm data from a service"}
+});
+
+myApp.filter('reverse', function() {
+    return function(text) {
+        return text.split("").reverse().join("");
+    }
+})
+myApp.filter('uCase', function() {
+    return function(text) {
+        return text.toUpperCase();
+    }
+})
+
+function FirstCtrl($scope, Data) {
+    $scope.data = Data;
+}
+
+
+function SecondCtrl($scope, Data) {
+    $scope.data = Data;
+
 }
