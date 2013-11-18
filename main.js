bb0017e1a74e924ef8c8e16f739ab298094898a6
Marvin Hoffman <divermarv@gmail.com>
2013-11-18 08:22:02 -0600

    Avengers Assembled!

--- main.js
+++ main.js
@@ -2,27 +2,74 @@
  * Created by marvinhoffman on 11/15/13.
  */
 var myApp = angular.module("myApp", []);
-myApp.factory('Data', function() {
-    return {message: "I'm data from a service"}
-});
 
-myApp.filter('reverse', function() {
-    return function(text) {
-        return text.split("").reverse().join("");
-    }
+myApp.factory('Avengers', function(){
+    var Avengers = {};
+    Avengers.cast = [
+        {
+            name: 'Robert Downey Jr.',
+            character: 'Tony Stark / Iron Man'
+        },
+        {
+            name: 'Chris Evans',
+            character: 'Steve Rogers / Captain America'
+        },
+        {
+            name: 'Mark Ruffalo',
+            character: 'Bruce Banner / The Hulk'
+        },
+        {
+            name: 'Chris Hemsworth',
+            character: 'Thor'
+        },
+        {
+            name: 'Scarlett Johansson',
+            character: 'Natasha Romanoff / Black Widow'
+        },
+        {
+            name: 'Jeremy Renner',
+            character: 'Clint Barton / Hawkeye'
+        },
+        {
+            name: 'Tom Hiddleston',
+            character: 'Loki'
+        },
+        {
+            name: 'Clark Gregg',
+            character: 'Agent Phil Coulson'
+        },
+        {
+            name: 'Cobie Smulders',
+            character: 'Agent Maria Hill'
+        },
+        {
+            name: 'Stellan Skarsgård',
+            character: 'Selvig'
+        },
+        {
+            name: 'Samuel L. Jackson',
+            character: 'Nick Fury'
+        },
+        {
+            name: 'Gwyneth Paltrow',
+            character: 'Pepper Potts'
+        },
+        {
+            name: 'Paul Bettany',
+            character: 'Jarvis (voice)'
+        },
+        {
+            name: 'Alexis Denisof',
+            character: 'The Other'
+        },
+        {
+            name: 'Tina Benko',
+            character: 'NASA Scientist'
+        }
+    ];
+    return Avengers;
 })
-myApp.filter('uCase', function() {
-    return function(text) {
-        return text.toUpperCase();
-    }
-})
 
-function FirstCtrl($scope, Data) {
-    $scope.data = Data;
-}
-
-
-function SecondCtrl($scope, Data) {
-    $scope.data = Data;
-
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
 }
